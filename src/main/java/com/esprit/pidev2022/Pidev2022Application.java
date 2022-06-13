37d780f6ce8e4d14aca6125f36475f67b8e8d960
Fares El Ouissi <fares.elouissi@esprit.tn>
2022-06-13 22:13:06 +0100

    Project structure

---
+++ src/main/java/com/esprit/pidev2022/Pidev2022Application.java
@@ -0,0 +1,13 @@
+package com.esprit.pidev2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pidev2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pidev2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/esprit/pidev2022/Pidev2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.pidev2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pidev2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
