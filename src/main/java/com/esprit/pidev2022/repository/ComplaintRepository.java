0ab868adda29ed6d84af7ed89e5d1f7fb3f7134c
El Ouissi Fares <fares.elouissi@esprit.tn>
2022-07-01 19:33:06 +0100

    add crud to complaint Service ,repo and Controller

--- src/main/java/com/esprit/pidev2022/repository/ComplaintRepository.java
+++ src/main/java/com/esprit/pidev2022/repository/ComplaintRepository.java
@@ -2,6 +2,8 @@ package com.esprit.pidev2022.repository;
 
 import com.esprit.pidev2022.entities.Complaint;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComplaintRepository extends CrudRepository<Complaint, Long> {
 }
