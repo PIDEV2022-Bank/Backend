c1fb38d9005fd7d6035574bbd9f80d846adb5d04
Rihem Mansouri <rihem.mansouri@esprit.tn>
2022-07-18 19:42:23 +0100

    repair mail

--- src/main/java/com/esprit/pidev2022/entities/MyConstants.java
+++ src/main/java/com/esprit/pidev2022/entities/MyConstants.java
@@ -2,10 +2,11 @@ package com.esprit.pidev2022.entities;
 
 public class MyConstants {
     // Replace with your email here:
-    public static final String MY_EMAIL = "piteambi2022@gmail.com";
+    public static final String MY_EMAIL ="mohamedbechir.lahmedi@esprit.tn";
+
 
     // Replace password!!
-    public static final String MY_PASSWORD = "Pi@bi2022";
+    public static final String MY_PASSWORD = "azerty123++";
 
     // And receiver!
     public static final String FRIEND_EMAIL = "rihem.mansouri@esprit.tn";
