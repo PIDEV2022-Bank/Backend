a507c2e4ae8d3677c317440dbba89c01b8f75e5d
El Ouissi Fares <fares.elouissi@esprit.tn>
2022-07-18 20:31:08 +0100

    change 127 to localhost

--- src/main/java/com/esprit/pidev2022/Controller/RequestController.java
+++ src/main/java/com/esprit/pidev2022/Controller/RequestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RestController
 @Transactional
 @RequestMapping("/Request")
-@CrossOrigin("http://127.0.0.1:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RequestController {
     @Autowired
     public JavaMailSender emailSender;
