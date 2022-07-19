9b8f04f67b9f00eb4fbf6ac31055a2d7e3992692
Rihem Mansouri <rihem.mansouri@esprit.tn>
2022-07-19 17:28:51 +0100

    fotumfrontend

--- Frontend/BankFrontend/src/app/app-routing.module.ts
+++ Frontend/BankFrontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { PostComponent } from './post/post.component';
 import { ForumComponent } from './forum/forum.component';
 import { TransactionComponent } from './user/transaction/transaction.component';
 
@@ -13,6 +16,9 @@ const routes: Routes = [
   },
 
    { path:'forum', component :ForumComponent},
+     {path:'addpost', component :AddPostComponent},
+     {path:'comment', component :AddCommentComponent},
+     {path:'post', component :PostComponent},
  
 
 ]
--- Frontend/BankFrontend/src/app/forum/forum.component.ts
+++ Frontend/BankFrontend/src/app/forum/forum.component.ts
@@ -1,15 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {ForumService} from '../core/service/forum.service';
+import {forum} from '../core/models/forum';
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrls: ['./forum.component.css']
 })
+
 export class ForumComponent implements OnInit {
+ r: string 
 
-  constructor() { }
+  
+     forums :forum[]
+     
+    constructor(private data: ForumService) {
+      
+     }
 
-  ngOnInit(): void {
+
+
+    ngOnInit(): void {
+      this.data.getAllForum().subscribe( 
+     (data: forum[]) => {this.forums= data ;
+    console.log(data)}
+     
+     );
+  
+      
+    }
+
+    
+
   }
 
-}
+ 
+
+
